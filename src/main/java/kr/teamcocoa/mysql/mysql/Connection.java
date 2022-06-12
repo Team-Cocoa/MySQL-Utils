bda27fc8a4aef460b30fc5f60a63cad13bb6dfc4
fixca <jaehoon1744@gmail.com>
2022-06-12 21:16:04 +0900

    Connection 끊김 방지 스레드 추가

--- src/main/java/kr/teamcocoa/mysql/mysql/Connection.java
+++ src/main/java/kr/teamcocoa/mysql/mysql/Connection.java
@@ -4,20 +4,36 @@ import lombok.Getter;
 import org.bukkit.Bukkit;
 
 import java.sql.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Getter
 public class Connection {
+    
     private String dbName;
     private java.sql.Connection connection;
+    private ExecutorService preventDisconnectTask;
 
     protected Connection(String dbName) {
         this.dbName = dbName;
+        this.preventDisconnectTask = Executors.newFixedThreadPool(1);
     }
 
     public void connect() {
         if (!isConnected()) {
             try {
                 this.connection = DriverManager.getConnection("jdbc:mysql://localhost/" + dbName, "root", "root");
+                this.preventDisconnectTask.execute(() -> {
+                    while(true) {
+                        try(    PreparedStatement preparedStatement = getPreparedStatement("select 1");
+                                ResultSet rs = preparedStatement.executeQuery()) {
+                            Thread.sleep(1000 * 60 * 60);
+                        }
+                        catch (SQLException | InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
                 Bukkit.getLogger().info("[MySQL] Successfully connected to MySQL(Database : " + dbName + ")!");
             } catch (SQLException e) {
                 e.printStackTrace();
